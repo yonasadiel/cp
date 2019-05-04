8b5364efe4d66fb9e8e5d1542009cd8946b4beb6
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-05-05 06:04:23 +0700

    ICPC Provincial 2017 by Maranatha University

---
+++ Contests/2017_Maranatha/Qual_ThreeDimensionalGrid.cpp
@@ -0,0 +1,17 @@
+/**
+ * Solution by Ensof Barhami
+ */
+#include<bits/stdc++.h>
+using namespace std;
+long long a[50][50];
+int x,y,z,i,j;
+int main(){
+    for(i=0;i<=43;i++){
+        for(j=0;j<=i;j++){
+            if(j=0 || j==i) a[i][j]=1;
+            else a[i][j]=a[i-1][j]+a[i-1][j-1];
+        }
+    }
+    cin>>x>>y>>z;
+    cout<<a[x+y+z][x]*a[y+z][y]<<endl;
+}
---
+++ Contests/2017_Maranatha/Trial_2_HaloMaranatha.cpp
@@ -0,0 +1,24 @@
+/**
+ * Solution by Ensof Barhami
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int h,a,l,o,sp,m,a1,r,a2,n,a3,t,h1,a4;
+int main(){
+    scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d",&h,&a,&l,&o,&sp,&m,&a1,&r,&a2,&n,&a3,&t,&h1,&a4);
+    for(int i=0;i<h;i++)cout<<"h";
+    for(int i=0;i<a;i++)cout<<"a";
+    for(int i=0;i<l;i++)cout<<"l";
+    for(int i=0;i<o;i++)cout<<"o";
+    for(int i=0;i<sp;i++)cout<<" ";
+    for(int i=0;i<m;i++)cout<<"m";
+    for(int i=0;i<a1;i++)cout<<"a";
+    for(int i=0;i<r;i++)cout<<"r";
+    for(int i=0;i<a2;i++)cout<<"a";
+    for(int i=0;i<n;i++)cout<<"n";
+    for(int i=0;i<a3;i++)cout<<"a";
+    for(int i=0;i<t;i++)cout<<"t";
+    for(int i=0;i<h1;i++)cout<<"h";
+    for(int i=0;i<a4;i++)cout<<"a";
+    cout<<endl;
+}
